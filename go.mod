5671452134a29c20b424caef2e440542a3132ead
Jose Alfredo Sitanggang <josealfredositanggang@gmail.com>
2020-10-21 05:33:00 +0700

    Added cors

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/talkwithcode-com/codex
 
 go 1.15
+
+require github.com/rs/cors v1.7.0
--- main.go
+++ main.go
@@ -5,6 +5,7 @@ import (
 	"net/http"
 	"os"
 
+	"github.com/rs/cors"
 	"github.com/talkwithcode-com/codex/engine"
 	"github.com/talkwithcode-com/codex/server"
 )
@@ -14,5 +15,6 @@ var port = os.Getenv("CODEX_PORT")
 func main() {
 	eg := engine.New(os.TempDir(), "main")
 	cs := server.New(eg)
-	log.Fatal(http.ListenAndServe(":"+port, cs))
+	handler := cors.Default().Handler(cs)
+	log.Fatal(http.ListenAndServe(":"+port, handler))
 }
